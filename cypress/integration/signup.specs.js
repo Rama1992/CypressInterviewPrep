fc436305606e7286b9ebb998806377d1cf2075f0
Rama1992 <ranantatmula@unomaha.edu>
2022-06-02 00:50:04 -0500

    adding more test cases(4)

--- cypress/integration/signup.specs.js
+++ cypress/integration/signup.specs.js
@@ -7,9 +7,10 @@ describe('Validating the sign up flow', () => {
     const dataTypes = ['Google Sheets', 'Paste table data', 'Microsoft Excel', 'CSV file'];
     const startBuildingButton = '.ml3';
     const imagePath = `/images/growth/celebration.png`;
-    const email = utilities.emailId();    
+    const email = utilities.emailId();
 
-    it('signing up without selecting any option in sign up flow', () => {
+    beforeEach(() => {
+        //launching the website
         cy.visit("https://www.airtable.com/");
         cy.clearCookies({domain: null});
         cy.findAllByText('Sign up for free').contains('Sign up for free').should('exist').click();
@@ -21,6 +22,9 @@ describe('Validating the sign up flow', () => {
         cy.findByText('Sign up with Google').should('exist');
         cy.findByText('Already have an account?').should('exist');
         cy.findByText('Sign in').should('exist').should('have.class', 'link');
+    })
+
+    it('signing up without selecting any option in sign up flow', () => {
 
         //signing up
         cy.findByLabelText('Work email address').should('exist').clear().type(email);
@@ -49,7 +53,7 @@ describe('Validating the sign up flow', () => {
         };
         recurse(
             () => cy.wrap(Cypress.$("body:contains('Skip')").length),
-            (len) => len === 0    
+            (len) => len === 1   
         )
 
         //testing start building page
@@ -63,4 +67,33 @@ describe('Validating the sign up flow', () => {
         cy.findAllByText('My First Workspace').contains('My First Workspace').should('exist');
     });
 
+    it('signing up with an existing email id', () => {
+        const emailId = 'r4@grr.la';
+        cy.findByLabelText('Work email address').should('exist').clear().type(emailId);
+        cy.findByText('Continue').should('exist').should('have.class', 'focusable').click();
+        cy.findAllByPlaceholderText('First and Last').should('exist').clear().type('Rama');
+        cy.get('#password').should('exist').clear().type('Testing123!');
+        cy.findByText('Continue').should('exist').should('have.class', 'focusable').click();
+        cy.findByText('Email already in use').should('exist').should('have.class', 'strong')
+            .parents('div').should('have.class', 'red');
+    });
+
+    it('signing up with invalid email id(missing @) should keep continue button in disabled state', () => {
+        const emailId = 'r4gmail.la';
+        cy.findByLabelText('Work email address').should('exist').clear().type(emailId);
+        cy.findByText('Continue').should('exist').should('not.have.class', 'focusable');
+    });
+
+    it('signing up with invalid email id(missing dot) should keep continue button in disabled state', () => {
+        const emailId = 'r4@gmailla';
+        cy.findByLabelText('Work email address').should('exist').clear().type(emailId);
+        cy.findByText('Continue').should('exist').should('not.have.class', 'focusable');
+    });
+
+    it('signing up with invalid email id, should throw an inline error message saying invalid email', () => {
+        const emailId = 'r4gmail.la';
+        cy.findByLabelText('Work email address').should('exist').clear().type(emailId);
+        cy.findByText('Continue').should('exist').should('not.have.class', 'focusable').click({force: true});
+        cy.findByText('Invalid email').should('exist').should('have.class', 'text-red');
+    });
 });
--- cypress/support/utilities.js
+++ cypress/support/utilities.js
@@ -1,5 +1,5 @@
 export const emailId = () => {
-    const randomNumber = `${Math.floor(Math.random() * 1000)}`;
+    const randomNumber = `${Math.floor(Math.random() * 10000)}`;
     const email = `rama${randomNumber}@grr.la`;
 
     return email;
